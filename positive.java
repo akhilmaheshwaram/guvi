046db2f7b0f912b36ba9f9f21f9d991008674fdd
akhilmaheshwaram <30789391+akhilmaheshwaram@users.noreply.github.com>
2017-08-07 09:58:17 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.Scanner;
+public class positive
 public Static void main(String args[])
+{
+try
 {
 int n;
 Scanner sc = new Scanner(System.in);
@@ -18,3 +21,9 @@ else
 System.out.println("0");
 }
 }
+        catch()
+        {
+          System.out.println("Please enter correct input");
+        }
+}
+        }
